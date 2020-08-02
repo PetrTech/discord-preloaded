aad9648d2fd77a50bcc2b48cb42a05960d3765c1
Petr Nedvěd <petrne09@gmail.com>
2020-08-02 07:12:05 +0000

    changed the branch

--- index.js
+++ index.js
@@ -21,12 +21,17 @@ Be sure to install java
 Be sure to install java JDK 8 or higher
 Be sure to install node.js to your PC
 
+----File structure setup
+create a new file for example on your desktop, open the file and click the place where you have the filepath and type 'cmd' instead of whatever you have there. This will open a command prompt window in the file location.
+Once you have opened command prompt in that location then type 'npm install discord.js'
+after installing, you can close the window
+
 ----Discord bot setup
 Be sure to have a discord bot client set up. Go to discord.com/developers. Click 'new application' and on the left go to bots and click 'create bot'. Edit it how you like
 Be sure to copy the bot's ID on the bots developer page in the BOTS tab.
 Paste the token in const TOKEN in the ''
 To run the bot just turn on your command prompt and go to the bot's code directory and type: 'node .' (without the ''s) and hit enter. It should run the bot.
-You can delete these comments after. (Delete only 3 - 32)
+You can delete these comments after. (Delete only 3 - 37)
 
 --------------==================INFO==================--------------
 */
